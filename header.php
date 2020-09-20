28409a1ec2b7bb46064c8c8a5b2afa94d5cc5d34
ddmcclure <ddmcclure@coastal.edu>
2020-09-20 14:09:44 -0400

    Created header and footer files

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Popflick - Home
+    </title>
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Epilogue:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Epilogue:wght@300;700&display=swap" rel="stylesheet">
+</head>
+<header>
+    <h1>Popflick</h1>
+</header>
+<body>
