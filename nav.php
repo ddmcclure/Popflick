52df111fe68054ca10eea466c7d1c74fe01cafcb
ddmcclure <ddmcclure@coastal.edu>
2020-09-20 14:31:39 -0400

    Rewrote all HTML files to PHP except for index

---
+++ nav.php
@@ -0,0 +1,9 @@
+<?php
+?>
+<div class="topnav">
+    <a href="index.html">Home</a>
+    <a href="catalog.html">Catalog</a>
+    <a class="active" href="members.html">Members</a>
+    <a href="storeinfo.html">About us</a>
+    <a href="contact.html">Contact Us</a>
+</div>
