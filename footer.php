346a514a724f4d00c7344de951e52b564a3904c1
btparke1 <btparke1@coastal.edu>
2020-09-23 14:10:24 -0400

    Commiting existing code to personal branch

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<footer>
+    <div class="footerLeft">Team 1 - Blakley Parker, Mary Chambers, Devin Mcclure</div>
+
+    <div class="footerRight">
+        <img src="img/Sweetgrass_facebook-01.png" alt="Logo" style="width:25%;height:25%;">
+        <img src="img/Sweetgrass_instagram-01.png" alt="Logo" style="width:25%;height:25%;">
+        <img src="img/Sweetgrass_twitter-01.png" alt="Logo" style="width:25%;height:25%;">
+    </div>
+</footer>
+</body>
+</html>
