9c49e9a4030ee6702bbc80db1456b8ae9c855ab4
ddmcclure <ddmcclure@coastal.edu>
2020-09-22 22:16:22 -0400

    Test database connection

--- members.php
+++ members.php
@@ -1,7 +1,24 @@
 <?php
 require_once "header.php";
+try {
+    $sql = "SELECT CusFName, CusLName FROM Customers";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<table>
+        <tr><th>First Name</th><th>Last Name</th></tr>";
+        foreach($row as $row) {
+            echo "<td>" . $row['CusFName'] . "</td>";
+            echo "<td>" . $row['CusLName'] . "</td>";
+            echo "</tr>\n";
+        }
+    echo "</table>";
+}
+catch (PDOException $e) {
+    die($e->getMessage());
+}
 ?>
 
 <?php
 require_once "footer.php";
-
