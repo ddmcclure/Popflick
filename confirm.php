0e9511056e78ad3a07347878d5e88f1f2e01e8a0
ddmcclure <ddmcclure@coastal.edu>
2020-10-17 13:51:12 -0400

    Small changes to confirm

--- confirm.php
+++ confirm.php
@@ -1,11 +1,11 @@
 <?php
 require_once "header.php";
 if ($_GET['state'] == 1) {
-    echo "You have successfully logged out. <br>";
-    echo "<a href='members.php'>Log in</a>";
+    echo "<p>You have successfully logged out.</p> <br>";
+    echo "<p><a href='members.php'>Log in</a></p>";
 }
 elseif ($_GET['state'] == 2) {
-    echo "Welcome $_SESSION[uname]";
+    echo "<p>Welcome $_SESSION[uname]!</p>";
 }
 else {
     echo "<h1>You are not supposed to be here.</h1>";
