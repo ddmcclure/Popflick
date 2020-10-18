6e6d88407e26be4885114d535ebc3b0aa06efc71
btparke1 <btparke1@coastal.edu>
2020-10-18 16:03:15 -0400

    Updated style and backend index

--- Backend/index.php
+++ Backend/index.php
@@ -76,34 +76,3 @@ function showSlides() {
   setTimeout(showSlides, 5000);
 }
 </script>";
-
-// echo "<script>
-// var slideIndex = 1;
-// showSlides(slideIndex);
-//
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-//
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-//
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName('mySlides');
-//   var dots = document.getElementsByClassName('dot');
-//   if (n > slides.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = 'none';
-//     }
-//     slideIndex++;
-//     for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(' active ', '');
-//     }
-//   slides[slideIndex-1].style.display = 'block';
-//   dots[slideIndex-1].className += ' active ';
-//   setTimeout(showSlides, 5000); // Change image every 5 seconds
-// }
-// </script>";
