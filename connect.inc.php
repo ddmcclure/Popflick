c7aeb0ded814e3200792ee02ab2f02c9b1e40a90
btparke1 <btparke1@coastal.edu>
2020-11-20 09:01:56 -0500

    Update connect.inc.php

--- connect.inc.php
+++ connect.inc.php
@@ -1,8 +1,8 @@
 <?php
 try {
-    $connString = "mysql:host=localhost;dbname=csci303fa19";
-    $user = "csci303fa19";
-    $pass = "csci303fa19!"; //normally would hide this information but i don't think it's that important
+    $connString = "mysql:host=localhost;dbname=popflick";
+    $user = "root";
+    $pass = "";
     $pdo = new PDO($connString,$user,$pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
